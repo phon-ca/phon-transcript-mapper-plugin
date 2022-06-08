d12a826a42e986f4753d59e2c4e1e8d0dd26e364
Greg Hedlund <greg.hedlund@gmail.com>
2022-06-08 09:06:59 -0230

    impl addAlignedMorphemes function for db

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # phon-morpheme-mapper-plugin
 
 Map data from one tier to other tiers based on aligned words/morphemes.
-
-
--- src/main/java/ca/phon/session/alignedMorpheme/MorphemeTaggerDatabase.java
+++ src/main/java/ca/phon/session/alignedMorpheme/MorphemeTaggerDatabase.java
@@ -1,6 +1,5 @@
 package ca.phon.session.alignedMorpheme;
 
-import bibliothek.gui.dock.common.location.CFlapIndexLocation;
 import ca.hedlund.tst.*;
 import ca.phon.app.log.LogUtil;
 import ca.phon.project.Project;
@@ -12,7 +11,7 @@ public class MorphemeTaggerDatabase {
 
 	private TernaryTree<TierDescription> tierDescriptionTree;
 
-	private TernaryTree<MorphemeTaggerEntry[]> tree;
+	private TernaryTree<Collection<MorphemeTaggerEntry>> tree;
 
 	public MorphemeTaggerDatabase(Project project) {
 		super();
@@ -51,7 +50,7 @@ public class MorphemeTaggerDatabase {
 	 * Add tier value to database without aligned tier data
 	 *
 	 */
-	public void addMorphemeForTier(String tierName, String morpheme) {
+	public TernaryTreeNode<Collection<MorphemeTaggerEntry>> addMorphemeForTier(String tierName, String morpheme) {
 		// ensure tier exists
 		TernaryTreeNode<TierDescription> tierNameRef = tierDescriptionTree.findNode(tierName);
 		if(tierNameRef == null) {
@@ -65,12 +64,40 @@ public class MorphemeTaggerDatabase {
 		if(tierNameRef == null)
 			throw new IllegalStateException("Unable to add tier name to database");
 
-		TernaryTreeNode<MorphemeTaggerEntry[]> morphemeNode = tree.findNode(morpheme);
-		if(morphemeNode == null) {
-			MorphemeTaggerEntry entry = new MorphemeTaggerEntry(tierNameRef, new LinkedHashMap<>());
+		TernaryTreeNode<Collection<MorphemeTaggerEntry>> morphemeNode = tree.findNode(morpheme, true, true);
+
+		if(!morphemeNode.isTerminated()) {
+			List<MorphemeTaggerEntry> entryList = new ArrayList<>();
+			morphemeNode.setValue(entryList);
+		}
+		MorphemeTaggerEntry entry = new MorphemeTaggerEntry(tierNameRef, new LinkedHashMap<>());
+		morphemeNode.getValue().add(entry);
+
+		return morphemeNode;
+	}
+
+	public void addAlignedMorphemes(Map<String, String> alignedMorphemes) {
+		for(var entry:alignedMorphemes.entrySet()) {
+			addMorphemeForTier(entry.getKey(), entry.getValue());
 		}
 
-
+		var entryList = alignedMorphemes.entrySet().toArray();
+		for(int i = 0; i < entryList.length; i++) {
+			var entry = (Map.Entry<String, String>)entryList[i];
+			TernaryTreeNode<Collection<MorphemeTaggerEntry>> morphemeNodeRef = tree.findNode(entry.getValue());
+			Optional<MorphemeTaggerEntry> morphemeEntryOpt =
+					morphemeNodeRef.getValue().stream().filter((e) -> e.getTierName().equals(entry.getKey())).findAny();
+			if (morphemeEntryOpt.isPresent()) {
+				MorphemeTaggerEntry morphemeEntryForTier = morphemeEntryOpt.get();
+				for(int j = 0; j < entryList.length; j++) {
+					if(j == i) continue;
+					var otherEntry = (Map.Entry<String, String>)entryList[j];
+					TernaryTreeNode<TierDescription> tierNodeRef = tierDescriptionTree.findNode(otherEntry.getValue());
+					TernaryTreeNode<Collection<MorphemeTaggerEntry>> otherNodeRef = tree.findNode(otherEntry.getValue());
+					morphemeEntryForTier.alignedTierMap.put(tierNodeRef, otherNodeRef);
+				}
+			}
+		}
 	}
 
 	public Collection<String> tierNames() {
@@ -82,47 +109,40 @@ public class MorphemeTaggerDatabase {
 	}
 
 	private MorphemeTaggerEntry morphemeEntryForTier(String key, String tierName) {
-		final MorphemeTaggerEntry[] entries = morphemeEntries(key);
+		final Collection<MorphemeTaggerEntry> entries = morphemeEntries(key);
 		final Optional<MorphemeTaggerEntry> morphemeTaggerEntry =
-			Arrays.stream(entries).filter((v) -> v.getTierName().equals(tierName)).findAny();
+			entries.stream().filter((v) -> v.getTierName().equals(tierName)).findAny();
 		if(morphemeTaggerEntry.isPresent())
 			return morphemeTaggerEntry.get();
 		else
 			return null;
 	}
 
-	private MorphemeTaggerEntry[] morphemeEntries(String key) {
-		final TernaryTreeNode<MorphemeTaggerEntry[]> node = tree.findNode(key);
-		if(node == null) return new MorphemeTaggerEntry[0];
+	private Collection<MorphemeTaggerEntry> morphemeEntries(String key) {
+		final TernaryTreeNode<Collection<MorphemeTaggerEntry>> node = tree.findNode(key);
+		if(node == null) return new ArrayList<>();
 
 		return node.getValue();
 	}
 
 	private class MorphemeTaggerEntry {
-		// reference to tier name node
+		// reference to tier name node, tier names are stored frequently
+		// and storing a reference to the tree node reduces memory footprint
 		TernaryTreeNode<TierDescription> tierNameRef;
 
-		// links to other tiers
-		Map<TernaryTreeNode<TierDescription>, TernaryTreeNode<MorphemeTaggerEntry>> alignedTierRefs;
+		// map of links to aligned tier data
+		Map<TernaryTreeNode<TierDescription>, TernaryTreeNode<Collection<MorphemeTaggerEntry>>> alignedTierMap;
 
 		public MorphemeTaggerEntry(TernaryTreeNode<TierDescription> tierNameRef,
-		                           Map<TernaryTreeNode<TierDescription>, TernaryTreeNode<MorphemeTaggerEntry>> alignedTierRefs) {
+		                           Map<TernaryTreeNode<TierDescription>, TernaryTreeNode<Collection<MorphemeTaggerEntry>>> alignedTierMap) {
 			super();
 
 			this.tierNameRef = tierNameRef;
-			this.alignedTierRefs = alignedTierRefs;
+			this.alignedTierMap = alignedTierMap;
 		}
 
 		public String getTierName() {
 			return this.tierNameRef.getPrefix();
-		}
-
-		public Map<String, String> getAlignedTierMorphemes() {
-			Map<String, String> retVal = new LinkedHashMap<>();
-			for(var alignedTierRef:this.alignedTierRefs.entrySet()) {
-				retVal.put(alignedTierRef.getKey().getPrefix(), alignedTierRef.getValue().getPrefix());
-			}
-			return retVal;
 		}
 
 	}
