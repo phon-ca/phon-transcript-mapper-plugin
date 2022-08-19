7c460188e917754ca1b5a78f61f199c365fa4ce4
Greg Hedlund <greg.hedlund@gmail.com>
2022-08-19 08:31:46 -0230

    Save changes to aligned types database on application exit

--- src/main/java/ca/phon/transcriptMapper/UserATDBShutdownHook.java
+++ src/main/java/ca/phon/transcriptMapper/UserATDBShutdownHook.java
@@ -1,13 +1,35 @@
 package ca.phon.transcriptMapper;
 
 import ca.phon.app.hooks.PhonShutdownHook;
-import ca.phon.plugin.PluginException;
+import ca.phon.app.log.LogUtil;
+import ca.phon.plugin.*;
 
-public class UserATDBShutdownHook implements PhonShutdownHook {
+import java.io.IOException;
+
+public class UserATDBShutdownHook implements PhonShutdownHook, IPluginExtensionPoint<PhonShutdownHook> {
 
 	@Override
 	public void shutdown() throws PluginException {
-		// TODO save changes if necessary
+		final UserATDB userATDB = UserATDB.getInstance();
+		if(userATDB.isATDBLoaded() && userATDB.isModified()) {
+			LogUtil.info("[TranscriptMapper] Saving user aligned types database");
+			try {
+				userATDB.saveDb();
+				LogUtil.info("[TranscriptMapper] Finished saving user aligned types database");
+			} catch (IOException e) {
+				LogUtil.warning(e);
+				throw new PluginException(e);
+			}
+		}
 	}
 
+	@Override
+	public Class<?> getExtensionType() {
+		return PhonShutdownHook.class;
+	}
+
+	@Override
+	public IPluginExtensionFactory<PhonShutdownHook> getFactory() {
+		return (args -> this);
+	}
 }
